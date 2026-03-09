92b419d14401d52ac7b33d0e7490ea0c0bc30f23
Varshitha Ruttala <varshitharuttala3@gmail.com>
2026-03-09 15:30:24 +0530

    Update app.js

--- client_dashboard/app.js
+++ client_dashboard/app.js
@@ -2,53 +2,61 @@ let chart;
 
 async function predict() {
 
-    const open = document.getElementById("open").value
-    const high = document.getElementById("high").value
-    const low = document.getElementById("low").value
-    const volume = document.getElementById("volume").value
 
-    const response = await fetch("http://127.0.0.1:5000/predict", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({
-            open: parseFloat(open),
-            high: parseFloat(high),
-            low: parseFloat(low),
-            volume: parseFloat(volume)
-        })
+const open = document.getElementById("open").value
+const high = document.getElementById("high").value
+const low = document.getElementById("low").value
+const volume = document.getElementById("volume").value
+
+const response = await fetch("http://127.0.0.1:5000/predict", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        open: parseFloat(open),
+        high: parseFloat(high),
+        low: parseFloat(low),
+        volume: parseFloat(volume)
     })
+})
 
-    const data = await response.json()
+const data = await response.json()
 
-    const predicted = data.predicted_price
-    const suggestion = data.suggestion
+console.log(data)
 
-    document.getElementById("result").innerText =
-        `Predicted Price: $${predicted} | Suggestion: ${suggestion}`
+const predicted = data.predicted_price || data.prediction
+const suggestion = predicted > open ? "BUY" : "SELL"
 
-    drawChart(predicted)
+document.getElementById("result").innerText =
+    `Predicted Price: $${predicted} | Suggestion: ${suggestion}`
+
+drawChart(predicted)
+
+
 }
 
-function drawChart(predicted) {
+function drawChart(predicted){
 
-    const ctx = document.getElementById("chart")
 
-    if(chart) chart.destroy()
+const ctx = document.getElementById("chart")
 
-    chart = new Chart(ctx, {
-        type: "line",
-        data: {
-            labels: ["Current", "Predicted"],
-            datasets: [{
-                label: "BTC Price",
-                data: [
-                    document.getElementById("open").value,
-                    predicted
-                ],
-                borderColor: "green"
-            }]
-        }
-    })
+if(chart) chart.destroy()
+
+chart = new Chart(ctx,{
+    type:"line",
+    data:{
+        labels:["Current","Predicted"],
+        datasets:[{
+            label:"BTC Price",
+            data:[
+                document.getElementById("open").value,
+                predicted
+            ],
+            borderColor:"green"
+        }]
+    }
+})
+
+
 }
