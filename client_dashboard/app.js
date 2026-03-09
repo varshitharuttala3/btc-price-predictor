72d1851429bd360ce64d582c6ce097bafb85eae6
Varshitha Ruttala <varshitharuttala3@gmail.com>
2026-03-09 15:10:38 +0530

    Update app.js

--- client_dashboard/app.js
+++ client_dashboard/app.js
@@ -1,5 +1,35 @@
 let chart
 
+async function predict(){
+
+const open = document.getElementById("open").value
+const high = document.getElementById("high").value
+const low = document.getElementById("low").value
+const volume = document.getElementById("volume").value
+
+const response = await fetch("http://127.0.0.1:5000/predict",{
+method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
+body:JSON.stringify({
+open:open,
+high:high,
+low:low,
+volume:volume
+})
+})
+
+const data = await response.json()
+
+document.getElementById("result").innerText =
+"Predicted Price: $" + data.predicted_price +
+" | Suggestion: " + data.suggestion
+
+drawChart(data.predicted_price)
+
+}
+
 function drawChart(predicted){
 
 const ctx = document.getElementById("chart")
@@ -12,7 +42,10 @@ data:{
 labels:["Current","Predicted"],
 datasets:[{
 label:"BTC Price",
-data:[document.getElementById("open").value,predicted],
+data:[
+document.getElementById("open").value,
+predicted
+],
 borderColor:"green"
 }]
 }
